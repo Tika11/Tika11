24b5f32d1a8ec14b2a83f5f28f9b7aa661893033
Tika11 <94659582+Tika11@users.noreply.github.com>
2021-11-19 10:26:22 +0700

    Delete introduce.py

--- introduce.py
+++
@@ -1,6 +0,0 @@
-Nama = "Tika Fitriyatun Hasanah"
-Nim = "212410102006"
-Alamat = "Ds. Majungan, Kec. Pademawu, Kab. Pamekasan, Madura."
-Kelompok = "Semantic"
-print( "Nama     :",Nama,"\n" + "Nim      :",Nim,"\n" + "Alamat   :",Alamat,"\n" + "kelompok :", Kelompok,"\n")
-
